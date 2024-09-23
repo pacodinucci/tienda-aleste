acb551564111ec4587f73cb7681531bd17712f07
pacodinucci <franciscoldinucci@gmail.com>
2024-09-23 16:26:44 -0300

    notify fix

--- app/api/notify/route.ts
+++ app/api/notify/route.ts
@@ -17,6 +17,7 @@ export async function POST(req: Request) {
       const payment = await mercadopago.payment.findById(parsedBody.data.id);
 
       if (payment.body.status_detail === "accredited") {
+        console.log("Pago acreditado, actualizando la orden...");
         const order = await db.order.update({
           where: {
             id: payment.body.metadata.order_id,
@@ -30,32 +31,32 @@ export async function POST(req: Request) {
         });
 
         // TODO: crear order de shipnow
-        const shippingDetails = order.shippingDetails as any;
+        // const shippingDetails = order.shippingDetails as any;
 
-        const orderData = {
-          external_reference: order.id,
-          ship_to: {
-            name: order.name,
-            last_name: "",
-            zip_code: shippingDetails?.zipCode,
-            address_line: shippingDetails?.address,
-            city: shippingDetails?.city,
-            state: shippingDetails?.region,
-            email: order.email,
-          },
-          items: order.orderItems.map((item: any) => ({
-            id: item.product.shipnowVariantId,
-            quantity: item.quantity,
-          })),
-        };
+        // const orderData = {
+        //   external_reference: order.id,
+        //   ship_to: {
+        //     name: order.name,
+        //     last_name: "",
+        //     zip_code: shippingDetails?.zipCode,
+        //     address_line: shippingDetails?.address,
+        //     city: shippingDetails?.city,
+        //     state: shippingDetails?.region,
+        //     email: order.email,
+        //   },
+        //   items: order.orderItems.map((item: any) => ({
+        //     id: item.product.shipnowVariantId,
+        //     quantity: item.quantity,
+        //   })),
+        // };
 
-        const shipnowResponse = await postShipnowOrder(orderData);
+        // const shipnowResponse = await postShipnowOrder(orderData);
 
-        if (!shipnowResponse) {
-          return new NextResponse("Error al crear la orden en Shipnow.", {
-            status: 400,
-          });
-        }
+        // if (!shipnowResponse) {
+        //   return new NextResponse("Error al crear la orden en Shipnow.", {
+        //     status: 400,
+        //   });
+        // }
 
         console.log("METADATA -->>> ", payment.body.metadata);
         payment.body.metadata.products.map(async (product: any) => {
